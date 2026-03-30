0d47c3aca8e0e3884476085dcacf6bb86b7b27bf
MM Yap <mryap2@up.edu.ph>
2026-03-30 23:12:44 +0800

    fix the back button for edit page

--- resources/views/dogs/edit.blade.php
+++ resources/views/dogs/edit.blade.php
@@ -2,12 +2,7 @@
 
 @section('content')
 
-<h2 class="text-xl font-semibold mb-4">Edit Dog
-    <a href="{{ route('dogs.index') }}"
-    class="bg-gray-300 px-4 py-2 rounded">
-        < Back
-    </a>
-</h2>
+<h2 class="text-xl font-semibold mb-4">Edit Dog</h2>
 
 <form action="{{ route('dogs.update', $dog) }}" method="POST" class="bg-white p-6 rounded-xl shadow space-y-4">
     @csrf
@@ -19,9 +14,18 @@
 
     <input type="text" name="temperament" value="{{ $dog->temperament }}" class="w-full border p-2 rounded">
 
-    <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
-        Update
-    </button>
+    <div class="flex gap-3">
+
+        <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+            Update
+        </button>
+
+        <a href="{{ route('dogs.index') }}"
+            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Back
+        </a>
+
+    </div>
 
 </form>
 
